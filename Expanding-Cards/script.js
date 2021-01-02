5e1bde53bed45988a9a38c11918e4516965ce437
Gourav <gore.auluck@gmail.com>
2021-01-01 22:32:14 -0800

    Expanding Cards

--- Expanding-Cards/script.js
+++ Expanding-Cards/script.js
@@ -1,9 +1,10 @@
 const panels = document.querySelectorAll('.panel')
-
+const frame = document.querySelector('.frame')
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
         removeActiveClasses()
         panel.classList.add('active')
+        frame.style.backgroundImage = panel.style.backgroundImage
     })
 })
 
