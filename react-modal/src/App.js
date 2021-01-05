d49df26928f0bde968ad87bb3f7f725d86e93bf1
Gourav <gore.auluck@gmail.com>
2021-01-04 23:57:48 -0800

    React-Modal

---
+++ react-modal/src/App.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import styled from 'styled-components'
+import { Modal } from './components/Modal';
+import { GlobalStyle } from './globalStyles';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  `;
+const Button = styled.button`
+  min-width: 100px;
+  padding: 16px 32px;
+  border-radius: 4px;
+  background: #141414;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer;
+  `;
+
+
+  function App() {
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => {
+      setShowModal(prev => !prev);
+    }
+
+  return (
+    <>
+      <Container>
+        <Button onClick={openModal}>I'm a modal</Button>
+        <Modal showModal={showModal} setShowModal={setShowModal} />
+        <GlobalStyle />
+      </Container>
+    </>
+  );
+}
+
+export default App;
