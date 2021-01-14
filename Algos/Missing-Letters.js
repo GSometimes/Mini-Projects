be37f6f5d1853681fd226c16d79132a15f1bc478
Gourav <gore.auluck@gmail.com>
2021-01-13 22:53:22 -0800

    Missing Letters

---
+++ Algos/Missing-Letters.js
@@ -0,0 +1,26 @@
+// Missing Letters
+
+// This algorithm focuses on finding the missing letter in the passed letter range and return it. 
+
+//If all letters are present in the range, return undefined.
+
+
+function letters(str) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz"
+    alphabet=alphabet.split("");
+
+    str=str.split("");
+
+    if(str[0] != 'a'){
+        return undefined;
+    }
+
+    for(let i=0; i < str.length; i++) {
+        if(str[i] != alphabet[i]){
+            return alphabet[i];
+        }
+    }
+    return str;
+}
+
+letters("abce")
