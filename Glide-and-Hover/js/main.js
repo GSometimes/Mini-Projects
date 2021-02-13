6680707d82cc1318be970d059a53b3bcfafc2743
Gourav <gore.auluck@gmail.com>
2021-02-12 22:04:36 -0800

    Glide and Hover

--- Glide-and-Hover/js/main.js
+++ Glide-and-Hover/js/main.js
@@ -73,12 +73,81 @@ document.addEventListener("DOMContentLoaded", () => {
         indices.forEach(index => index.classList.remove("active"));
         indices[currentIndex].classList.add("active");
         distortAnimations[prevIndex].next();
+        showTextAnimation("next");
+        setTimeout(() => {
+            let canvas = background.querySelectorAll("canvas");
+            background.appendChild(canvas[0]);
+            distortAnimations[prevIndex].previous();
+        }, 500)
+    }
+
+
+    function startPrevDistortAnimation() {
+        currentIndex = currentIndex - 1 < 0 ? 3 : currentIndex - 1;
+        indices.forEach(index => index.classList.remove("active"));
+        indices[currentIndex].classList.add("active");
+        distortAnimations[currentIndex].next();
+        showTextAnimation("prev");
+        setTimeout(() => {
+            let canvas = background.querySelectorAll("canvas");
+            background.insertBefore(canvas[canvas.length - 1], background.firstChild);
+            distortAnimations[currentIndex].previous();
+        }, 500)
     }
 
     nextBtn.addEventListener("click", () => {
         startNextDistortAnimation();
-    })
+    });
 
+    prevBtn.addEventListener("click", () => {
+        startPrevDistortAnimation();
+    });
     
+
+    let titleDisplacement = 0;
+    let descriptionDisplacement = 0;
+
+    function showTextAnimation(direction) {
+        if(titleDisplacement === 0 && direction === "prev") {
+            titleDisplacement = -540;
+        } else if(titleDisplacement === -540 && direction === "next") {
+            titleDisplacement = 0;
+        } else {
+            titleDisplacement = (direction === "next") ? titleDisplacement - 180 : titleDisplacement + 180;
+        }
+
+        if(descriptionDisplacement === 0 && direction === "prev") {
+            descriptionDisplacement = -165;
+        } else if(descriptionDisplacement === -165 && direction === "next") {
+            descriptionDisplacement = 0;
+        } else {
+            descriptionDisplacement = (direction === "next") ? descriptionDisplacement - 55 : descriptionDisplacement + 55;
+        }
+
+        let title = document.querySelectorAll("#title h4");
+        let description = document.querySelectorAll("#description p");
+
+        title.forEach(title => {
+            TweenMax.to(title, 1, {
+                top: `${titleDisplacement}px`,
+                ease: Strong.easeInOut
+            });
+        });
+
+        description.forEach((description, index) =>{
+            let opacity = 0;
+            if(index === currentIndex) {
+                opacity = 1;
+            } else {
+                opacity = 0;
+            }
+            TweenMax.to(description, 1, {
+                top: `${descriptionDisplacement}px`,
+                ease: Strong.easeInOut,
+                opacity: opacity
+            });
+        })
+
+    }
 
 });
