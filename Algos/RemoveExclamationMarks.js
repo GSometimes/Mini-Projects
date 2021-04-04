145501b3fe40a8cc1a0ce49c0a8cbed3ee7caa21
Gourav <gore.auluck@gmail.com>
2021-04-03 20:55:49 -0700

    Remove Exclamation Marks

--- Algos/RemoveExclamationMarks.js
+++ Algos/RemoveExclamationMarks.js
@@ -2,11 +2,11 @@
 
 // Write a function which removes all exclamation marks from a given string.
 
-// function removeExclamationMarks(s) {
-//     let newString = '';
+// Define empty string
+// Create a for loop to iterate through string
+// If the index of the string doesn't equal an "!" then push that value to newString
+// Return newString
 
-    
-// }
 
 function removeExclamationMarks(s) {
     let newString = ''
