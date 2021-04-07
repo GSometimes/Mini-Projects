5728ffa641bf7f2a547a64360179beae355c04e9
Gourav <gore.auluck@gmail.com>
2021-04-06 20:53:59 -0700

    Pre-Fizzbuzz Workout #1

---
+++ Algos/Pre-FizzBuzz-Workout1.js
@@ -0,0 +1,27 @@
+// Pre-Fizzbuzz Workout #1
+
+// Write a function that gets you from the input to the output
+// Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+// Your expected output is an array of positive integers from 1 to n (inclusive).
+
+// Define an empty array
+// Create a loop that will increment from the value of n
+// Push those values into the empty array
+// Return new array
+
+
+function preFizz(n) {
+    let fizzed = []
+
+    for(i = 1; i <= n; i++){
+        if(i <= n){
+            fizzed.push(i)
+        }
+    }
+
+    return fizzed;
+}
+
+console.log(preFizz(1));
+console.log(preFizz(3));
+console.log(preFizz(5));
