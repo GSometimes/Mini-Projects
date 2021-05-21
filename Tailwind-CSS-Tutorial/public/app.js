4efa0d6d6d91333e13dacd79b542ab7acb372a6f
Gourav <gore.auluck@gmail.com>
2021-05-21 15:14:26 -0700

    gradient update

--- Tailwind-CSS-Tutorial/public/app.js
+++ Tailwind-CSS-Tutorial/public/app.js
@@ -1,38 +1,46 @@
-const bars = document.querySelector('#bars')
-const menu = document.querySelector('#menu')
+const bars = document.querySelector("#bars");
+const menu = document.querySelector("#menu");
 
-bars.addEventListener('click', () => {
-    if(menu.classList.contains('hidden') && window.innerWidth < 768) {
-        menu.classList.remove('hidden');
-        menu.classList.add(
-            'flex',
-            'flex-col',
-            'text-center',
-            'bg-purple-600',
-            'w-full',
-            'absolute',
-            'top-16'
-        )
-    } else {
-        menu.classList.add('hidden');
-    }
+bars.addEventListener("click", () => {
+  if (menu.classList.contains("hidden") && window.innerWidth < 768) {
+    menu.classList.remove("hidden");
+    menu.classList.add(
+      "flex",
+      "flex-col",
+      "text-center",
+      "bg-gradient-to-r",
+      "from-blue-600",
+      "via-green-500",
+      "to-yellow-400",
+      "w-full",
+      "absolute",
+      "top-16"
+    );
+  } else {
+    menu.classList.add("hidden");
+  }
 });
 
-menu.addEventListener('click', () => {
-    menu.classList.add('hidden')
-})
+// hover:text-purple-600 duration-300
 
-window.addEventListener('resize', () => {
-    if(window.innerWidth > 768) {
-        menu.classList.add('hidden')
-        menu.classList.remove(
-            'flex',
-            'flex-col',
-            'text-center',
-            'bg-purple-600',
-            'w-full',
-            'absolute',
-            'top-16'
-        )
-    }
-})
+menu.addEventListener("click", () => {
+  menu.classList.add("hidden");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    menu.classList.add("hidden");
+    menu.classList.remove(
+      "flex",
+      "flex-col",
+      "text-center",
+      "bg-gradient-to-r",
+      "from-blue-600",
+      "via-green-500",
+      "to-yellow-400",
+      "w-full",
+      "absolute",
+      "top-16"
+    );
+  }
+});
