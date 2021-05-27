cb443a0e268b27d7337b1839e3ea35468e183923
Gourav <gore.auluck@gmail.com>
2021-05-26 23:26:19 -0700

    Updates

--- tailwindcss-react/src/components/Dropdown.js
+++ tailwindcss-react/src/components/Dropdown.js
@@ -5,16 +5,16 @@ import {Link} from 'react-router-dom'
 const Dropdown = ({isOpen, toggle}) => {
     return (
         <div className={isOpen ? "grid grid-rows-4 text-center items-center bg-red-800" : "hidden"} onClick={toggle}>
-          <Link className="p-4" to="/">
+          <Link className="p-4 hover:text-white" to="/">
             Home
           </Link>
-          <Link className="p-4" to="/menu">
+          <Link className="p-4 hover:text-white" to="/menu">
             Menu
           </Link>
-          <Link className="p-4" to="/about">
+          <Link className="p-4 hover:text-white" to="/about">
             About
           </Link>
-          <Link className="p-4" to="/contact">
+          <Link className="p-4 hover:text-white" to="/contact">
             Contact
           </Link>                    
         </div>
